b2fbf1a1ce408513bea392cd95c1509099c9cd8a
realsumit <sumitsarinofficial@gmail.com>
2017-09-25 04:37:48 +0530

    249

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var bodyParser = require("body-parser")
 var campgrounds = [
 		{name:"salmon creek", image:"http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx"},
 		{name:"dinosaur park", image:"http://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/10/main/hoodview-campground-0510.jpg?itok=B8Eb65Uf"},
+		{name:"jackson creek", image:"http://www.ride2guide.com/CirclePinesKOA.jpg"},
+		{name:"salmon creek", image:"http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx"},
+		{name:"dinosaur park", image:"http://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/10/main/hoodview-campground-0510.jpg?itok=B8Eb65Uf"},
 		{name:"jackson creek", image:"http://www.ride2guide.com/CirclePinesKOA.jpg"}
 	];
 
