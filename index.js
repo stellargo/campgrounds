c2b91511a9dd499089cf5cb0630532525fc6f9c8
realsumit <sumitsarinofficial@gmail.com>
2017-09-27 03:17:11 +0530

    250

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser")
+var express 	= require("express"),
+	app 		= express(),
+	bodyParser 	= require("body-parser"),
+	mongoose 	= require("mongoose");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+//schema
+var campgroundschema = new mongoose.Schema({
+	name:String,
+	image:String
+});
+var Campground = mongoose.model("Campground",campgroundschema);
+
+Campground.create(
+	{
+		name:"salmon creek", 
+		image:"http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx"
+	}, function(err,campground){
+		if (err){
+			console.log(err);
+		}else{
+			console.log("added");
+		}
+	}
+);
 
 var campgrounds = [
 		{name:"salmon creek", image:"http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx"},
@@ -10,10 +35,6 @@ var campgrounds = [
 		{name:"dinosaur park", image:"http://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/10/main/hoodview-campground-0510.jpg?itok=B8Eb65Uf"},
 		{name:"jackson creek", image:"http://www.ride2guide.com/CirclePinesKOA.jpg"}
 	];
-
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.set("view engine","ejs");
 
 app.get("/",function(req,res){
 	res.render("landing")
