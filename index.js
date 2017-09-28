bab0605e74cb09b4daed2608b9805d57b2f00717
realsumit <sumitsarinofficial@gmail.com>
2017-09-28 20:45:27 +0530

    258

--- index.js
+++ index.js
@@ -14,43 +14,44 @@ var campgroundschema = new mongoose.Schema({
 });
 var Campground = mongoose.model("Campground",campgroundschema);
 
-Campground.create(
-	{
-		name:"salmon creek", 
-		image:"http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx"
-	}, function(err,campground){
-		if (err){
-			console.log(err);
-		}else{
-			console.log("added");
-		}
-	}
-);
-
-var campgrounds = [
-		{name:"salmon creek", image:"http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx"},
-		{name:"dinosaur park", image:"http://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/10/main/hoodview-campground-0510.jpg?itok=B8Eb65Uf"},
-		{name:"jackson creek", image:"http://www.ride2guide.com/CirclePinesKOA.jpg"},
-		{name:"salmon creek", image:"http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx"},
-		{name:"dinosaur park", image:"http://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/10/main/hoodview-campground-0510.jpg?itok=B8Eb65Uf"},
-		{name:"jackson creek", image:"http://www.ride2guide.com/CirclePinesKOA.jpg"}
-	];
+// Campground.create(
+// 	{
+// 		name:"dinosaur park", 
+// 		image:"http://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/10/main/hoodview-campground-0510.jpg?itok=B8Eb65Uf"
+// 	}, function(err,campground){
+// 		if (err){
+// 			console.log(err);
+// 		}else{
+// 			console.log(campground);
+// 		}
+// 	}
+// );
 
 app.get("/",function(req,res){
 	res.render("landing")
 });
 
 app.get("/campgrounds",function(req,res){
-	
-	res.render("campgrounds",{campgrounds:campgrounds});
+	Campground.find({},function(err,allCampgrounds){
+		if (err){
+			console.log(err);
+		}else{
+			res.render("campgrounds",{campgrounds:allCampgrounds});
+		}
+	})
 });
 
 app.post("/campgrounds",function(req,res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-	res.redirect("/campgrounds");
+	Campground.create(newCampground,function(err,newlyCreated){
+		if (err){
+			console.log(err);
+		}else{
+			res.redirect("/campgrounds");
+		}
+	});
 });
 
 app.get("/campgrounds/new",function(req,res){
